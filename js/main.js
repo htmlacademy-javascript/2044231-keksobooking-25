709b582d34bc1f1acf9ded19ae9da9b1a8063cad
OksanaPugach <oksana.pugach.it@gmail.com>
2022-11-20 15:48:59 +0100

    updated js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 function randomNumber (min, max, numOfDecimals ){
-    return +((Math.random() * (max - min + 1)) + min).toFixed(numOfDecimals);
+  return +((Math.random() * (max - min + 1)) + min).toFixed(numOfDecimals);
 }
 randomNumber();
